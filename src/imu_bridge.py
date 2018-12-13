9fc463c696403b45a9c36afe6a5cabf9d7522423
Unknown <carfi.alessandro@gmail.com>
2018-12-13 18:23:04 +0100

    msg process update

--- src/imu_bridge.py
+++ src/imu_bridge.py
@@ -9,59 +9,60 @@ import signal
 class imu_bridge(bridge.bridge):
 
     def msg_process(self, msg):
-        msg_list = msg.split(";")
-        
-        pkgSizeAcc = int(msg_list[0])
-        moduleSizeAcc = 4*pkgSizeAcc+2
-        acc_msg = msg_list[2:moduleSizeAcc]
+        msg_topic = msg.topic.split("/")
+        if(msg_topic[-1] == "imu"):
+            topic_name = msg_topic[0].replace(" ", "_")
+            msg_list = msg.payload.split(";")
+            
+            pkgSizeAcc = int(msg_list[0])
+            moduleSizeAcc = 4*pkgSizeAcc+2
+            acc_msg = msg_list[2:moduleSizeAcc]
 
-        pkgSizeGyro = int(msg_list[moduleSizeAcc])
-        moduleSizeGyro = 4*pkgSizeGyro+2
-        gyro_msg = msg_list[moduleSizeAcc+2:moduleSizeAcc+moduleSizeGyro]
+            pkgSizeGyro = int(msg_list[moduleSizeAcc])
+            moduleSizeGyro = 4*pkgSizeGyro+2
+            gyro_msg = msg_list[moduleSizeAcc+2:moduleSizeAcc+moduleSizeGyro]
 
-        biggerpkg = max([pkgSizeAcc, pkgSizeGyro])
+            biggerpkg = max([pkgSizeAcc, pkgSizeGyro])
 
-        imu_list = []
-        acceleration = Vector3()
-        velocity = Vector3()
+            acceleration = Vector3()
+            velocity = Vector3()
 
-        for i in range(0,biggerpkg):
-            time = ""
-            if i < pkgSizeGyro:
-                velocity = Vector3()
-                velocity.x = float(gyro_msg[3*i].replace(',','.'))
-                velocity.y = float(gyro_msg[3*i+1].replace(',','.'))
-                velocity.z = float(gyro_msg[3*i+2].replace(',','.'))
-                time = gyro_msg[3*pkgSizeGyro+i]
+            for i in range(0,biggerpkg):
+                time = ""
+                if i < pkgSizeGyro:
+                    velocity = Vector3()
+                    velocity.x = float(gyro_msg[3*i].replace(',','.'))
+                    velocity.y = float(gyro_msg[3*i+1].replace(',','.'))
+                    velocity.z = float(gyro_msg[3*i+2].replace(',','.'))
+                    time = gyro_msg[3*pkgSizeGyro+i]
 
-            if i < pkgSizeAcc:
-                acceleration = Vector3()
-                acceleration.x = float(acc_msg[3*i].replace(',','.'))
-                acceleration.y = float(acc_msg[3*i+1].replace(',','.'))
-                acceleration.z = float(acc_msg[3*i+2].replace(',','.'))
-                time = acc_msg[3*pkgSizeAcc+i]
+                if i < pkgSizeAcc:
+                    acceleration = Vector3()
+                    acceleration.x = float(acc_msg[3*i].replace(',','.'))
+                    acceleration.y = float(acc_msg[3*i+1].replace(',','.'))
+                    acceleration.z = float(acc_msg[3*i+2].replace(',','.'))
+                    time = acc_msg[3*pkgSizeAcc+i]
 
 
-            imu_message = Imu()
+                imu_message = Imu()
 
-            now = rospy.get_rostime()
-            imu_message.header.stamp.secs = now.secs
-            imu_message.header.stamp.nsecs = now.nsecs
+                now = rospy.get_rostime()
+                imu_message.header.stamp.secs = now.secs
+                imu_message.header.stamp.nsecs = now.nsecs
 
-            imu_message.header.frame_id = time
-            imu_message.angular_velocity = velocity
-            imu_message.linear_acceleration = acceleration
-            imu_list.append(imu_message)
-        return imu_list
+                imu_message.header.frame_id = time
+                imu_message.angular_velocity = velocity
+                imu_message.linear_acceleration = acceleration
+
+                imu_publisher = rospy.Publisher(topic_name, Imu, queue_size=1)
+                imu_publisher.publish(imu_message)
+        else:
+            print  msg.topic + " is not a supported topic"
 
 
 def main():
-    rospy.init_node('imu_bridge', anonymous=True)
-    device = rospy.get_param('~device_name','sensors')
-    mqtt_topic = device + '/imu'
-    print mqtt_topic
-    imu_publisher = rospy.Publisher('/imu_data', Imu, queue_size=1)
-    imu_sub = imu_bridge(imu_publisher, mqtt_topic, 'bridge_imu_'+ device)
+    rospy.init_node('imu_bridge', anonymous=True) 
+    imu_sub = imu_bridge('#', 'bridge_imu')
     rospy.on_shutdown(imu_sub.hook)
 
     while not rospy.is_shutdown():
--- src/imu_bridge_recordings.py
+++ src/imu_bridge_recordings.py
@@ -8,50 +8,50 @@ import signal
 class imu_bridge_recordings(bridge.bridge):
 
     def msg_process(self, msg):
-        msg_list = msg.split(";")
-        
-        pkgSizeAcc = int(msg_list[0])
-        moduleSizeAcc = 4*pkgSizeAcc+2
-        acc_msg = msg_list[2:moduleSizeAcc]
+        msg_topic = msg.topic.split("/")
+        if(msg_topic[-1] == "imu"):
+            topic_name = msg_topic[0].replace(" ", "_")
+            msg_list = msg.payload.split(";")
+            
+            pkgSizeAcc = int(msg_list[0])
+            moduleSizeAcc = 4*pkgSizeAcc+2
+            acc_msg = msg_list[2:moduleSizeAcc]
 
-        pkgSizeGyro = int(msg_list[moduleSizeAcc])
-        moduleSizeGyro = 4*pkgSizeGyro+2
-        gyro_msg = msg_list[moduleSizeAcc+2:moduleSizeAcc+moduleSizeGyro]
+            pkgSizeGyro = int(msg_list[moduleSizeAcc])
+            moduleSizeGyro = 4*pkgSizeGyro+2
+            gyro_msg = msg_list[moduleSizeAcc+2:moduleSizeAcc+moduleSizeGyro]
 
-        imu_data = ImuPackage()
+            imu_data = ImuPackage()
 
-        for i in range(0,pkgSizeAcc):
-            acceleration = Vector3Time()
-            acceleration.vector.x = float(acc_msg[3*i].replace(',','.'))
-            acceleration.vector.y = float(acc_msg[3*i+1].replace(',','.'))
-            acceleration.vector.z = float(acc_msg[3*i+2].replace(',','.'))
+            for i in range(0,pkgSizeAcc):
+                acceleration = Vector3Time()
+                acceleration.vector.x = float(acc_msg[3*i].replace(',','.'))
+                acceleration.vector.y = float(acc_msg[3*i+1].replace(',','.'))
+                acceleration.vector.z = float(acc_msg[3*i+2].replace(',','.'))
 
-            acceleration.time.data = long(acc_msg[3*pkgSizeAcc+i])
-            imu_data.linear_acceleration.append(acceleration)
+                acceleration.time.data = long(acc_msg[3*pkgSizeAcc+i])
+                imu_data.linear_acceleration.append(acceleration)
+                
 
-        for i in range(0,pkgSizeGyro):
-            velocity = Vector3Time()
-            velocity.vector.x = float(gyro_msg[3*i].replace(',','.'))
-            velocity.vector.y = float(gyro_msg[3*i+1].replace(',','.'))
-            velocity.vector.z = float(gyro_msg[3*i+2].replace(',','.'))
+            for i in range(0,pkgSizeGyro):
+                velocity = Vector3Time()
+                velocity.vector.x = float(gyro_msg[3*i].replace(',','.'))
+                velocity.vector.y = float(gyro_msg[3*i+1].replace(',','.'))
+                velocity.vector.z = float(gyro_msg[3*i+2].replace(',','.'))
 
-            velocity.time.data = long(gyro_msg[3*pkgSizeGyro+i])
-            imu_data.angular_velocity.append(velocity)
+                velocity.time.data = long(gyro_msg[3*pkgSizeGyro+i])
+                imu_data.angular_velocity.append(velocity)
 
-        return imu_data
+            imu_publisher = rospy.Publisher(topic_name+"_package", ImuPackage, queue_size=1)
+            imu_publisher.publish(imu_data)
+        else:
+            print  msg.topic + " is not a supported topic"
 
 
 def main():
     rospy.init_node('imu_bridge_recordings', anonymous=True)
-    device = rospy.get_param('~device_name','sensors')
-    mqtt_topic = device + '/imu'
-    print mqtt_topic
-    imu_publisher = rospy.Publisher('/imu_data', ImuPackage, queue_size=1)
-    imu_sub = imu_bridge_recordings(imu_publisher, mqtt_topic, 'bridge_imu_'+ device)
+    imu_sub = imu_bridge_recordings('#', 'bridge_imu_recording')
     rospy.on_shutdown(imu_sub.hook)
-
-    
-
 
     while not rospy.is_shutdown():
         imu_sub.looping()
